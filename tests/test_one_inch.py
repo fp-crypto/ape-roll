3c0b6272b4d1b95bf4e247f39ff3c81e7e017d99
FP <83050944+fp-crypto@users.noreply.github.com>
2023-08-30 10:31:11 -0700

    feat: fix overloads (#4)
    * feat: fix overloads
    * fix: one inch test

--- tests/test_one_inch.py
+++ tests/test_one_inch.py
@@ -1,19 +1,25 @@
 from ape import Contract, accounts, convert
-import pytest
+from hexbytes import HexBytes
 from ape_roll import WeirollPlanner
+import pytest
+import os
 import requests
 
+ONE_INCH_API_KEY = (
+    os.environ["ONE_INCH_API_KEY"] if "ONE_INCH_API_KEY" in os.environ else None
+)
 
-@pytest.mark.skip("FIXME: update 1inch api")
+
+@pytest.mark.skipif(ONE_INCH_API_KEY is None, reason="Need 1inch api key")
 def test_one_inch(weiroll_vm):
     whale = accounts["0x57757E3D981446D585Af0D9Ae4d7DF6D64647806"]
     weth = Contract("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
     crv = Contract("0xD533a949740bb3306d119CC777fa900bA034cd52")
-    one_inch = Contract("0x1111111254fb6c44bAC0beD2854e76F90643097d")
 
     weth.transfer(weiroll_vm.address, convert("10 ether", int), sender=whale)
 
-    swap_url = "https://api.1inch.io/v4.0/1/swap"
+    swap_url = f"https://api.1inch.dev/swap/v5.2/1/swap"
+    headers = {"Authorization": f"Bearer {ONE_INCH_API_KEY}"}
     r = requests.get(
         swap_url,
         params={
@@ -25,19 +31,21 @@ def test_one_inch(weiroll_vm):
             "disableEstimate": "true",
             "allowPartialFill": "false",
         },
+        headers=headers
     )
 
     assert r.ok and r.status_code == 200
     tx = r.json()["tx"]
+    one_inch = Contract(tx["to"])
 
-    weth.approve(one_inch, 2 ** 256 - 1, {"from": weiroll_vm, "gas_price": 0})
+    weth.approve(one_inch, 2 ** 256 - 1, sender=weiroll_vm)
 
-    decoded = one_inch.decode_input(tx["data"])
+    decoded = one_inch.decode_input(HexBytes(tx["data"]))
     func_name = decoded[0]
     params = decoded[1]
 
     planner = WeirollPlanner(weiroll_vm)
-    planner.call(one_inch, func_name, *params)
+    planner.call(one_inch, func_name, *params.values())
 
     cmds, state = planner.plan()
     weiroll_tx = weiroll_vm.execute(cmds, state, sender=whale)
