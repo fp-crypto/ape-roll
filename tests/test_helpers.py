a4e2a9fc31d4cd0a89c9ad6b162203640d5f98a2
FP <fp_crypt@protonmail.com>
2023-09-05 11:48:01 -0700

    chore: black

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -16,19 +16,25 @@ def test_insert(tuple_helper, alice):
 
     assert (
         HexBytes(
-            tuple_helper.insertElement.transact(b2 + b1, 0, b4, False, sender=alice).return_value
+            tuple_helper.insertElement.transact(
+                b2 + b1, 0, b4, False, sender=alice
+            ).return_value
         )
         == b4 + b2 + b1
     )
     assert (
         HexBytes(
-            tuple_helper.insertElement.transact(b2 + b1, 1, b4, False, sender=alice).return_value
+            tuple_helper.insertElement.transact(
+                b2 + b1, 1, b4, False, sender=alice
+            ).return_value
         )
         == b2 + b4 + b1
     )
     assert (
         HexBytes(
-            tuple_helper.insertElement.transact(b2 + b1, 2, b4, False, sender=alice).return_value
+            tuple_helper.insertElement.transact(
+                b2 + b1, 2, b4, False, sender=alice
+            ).return_value
         )
         == b2 + b1 + b4
     )
@@ -42,7 +48,9 @@ def test_insert(tuple_helper, alice):
 
     for i in range(101):
         r = HexBytes(
-            tuple_helper.insertElement.transact(rands, i, b4, False, sender=alice).return_value
+            tuple_helper.insertElement.transact(
+                rands, i, b4, False, sender=alice
+            ).return_value
         )
         inserted = HexBytes(rands[: i * 32] + HexBytes(b4) + rands[i * 32 :])
         assert r == inserted
@@ -52,13 +60,17 @@ def test_replace(tuple_helper, alice):
 
     assert (
         HexBytes(
-            tuple_helper.replaceElement.transact(b2 + b1, 0, b4, False, sender=alice).return_value
+            tuple_helper.replaceElement.transact(
+                b2 + b1, 0, b4, False, sender=alice
+            ).return_value
         )
         == b4 + b1
     )
     assert (
         HexBytes(
-            tuple_helper.replaceElement.transact(b2 + b1, 1, b4, False, sender=alice).return_value
+            tuple_helper.replaceElement.transact(
+                b2 + b1, 1, b4, False, sender=alice
+            ).return_value
         )
         == b2 + b4
     )
@@ -72,7 +84,9 @@ def test_replace(tuple_helper, alice):
 
     for i in range(100):
         r = HexBytes(
-            tuple_helper.replaceElement.transact(rands, i, b4, False, sender=alice).return_value
+            tuple_helper.replaceElement.transact(
+                rands, i, b4, False, sender=alice
+            ).return_value
         )
         inserted = HexBytes(rands[: i * 32] + HexBytes(b4) + rands[(i + 1) * 32 :])
         assert r == inserted
